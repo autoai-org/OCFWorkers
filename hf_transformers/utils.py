a4a183e93e7ac18b6e119d8bc3415f2808e7c402
Xiaozhe Yao <askxzyao@gmail.com>
2023-06-27 20:43:04 +0200

    update pynvml

--- hf_transformers/utils.py
+++ hf_transformers/utils.py
@@ -1,11 +1,19 @@
+import os
 from loguru import logger
+
 def get_visible_gpus_specs():
+    # https://github.com/gpuopenanalytics/pynvml/issues/28
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     gpus = []
     try:
         from pynvml import nvmlInit, nvmlDeviceGetCount, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, nvmlDeviceGetName
         nvmlInit()
-        deviceCount = nvmlDeviceGetCount()
-        for i in range(deviceCount):
+        if "CUDA_VISIBLE_DEVICES" in os.environ:
+            ids = list(map(int, os.environ.get("CUDA_VISIBLE_DEVICES", "").split(",")))
+        else:
+            deviceCount = nvmlDeviceGetCount()
+            ids = range(deviceCount)
+        for i in ids:
             handle = nvmlDeviceGetHandleByIndex(i)
             meminfo = nvmlDeviceGetMemoryInfo(handle)
             gpus.append({
